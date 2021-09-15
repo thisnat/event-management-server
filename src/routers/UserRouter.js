52096faf10a06bdc804d970966ded8dbead69a1b
Nattawut <thisnattawut@gmail.com>
2021-09-15 21:16:30 +0700

    hash password with bcrypt

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-
 const userSchema = require('../models/User');
+
+const bcrypt = require('bcrypt');
 
 router.route('/').get((req, res, next) => {
     userSchema.find((error, data) => {
@@ -14,7 +15,14 @@ router.route('/').get((req, res, next) => {
     });
 });
 
-router.route('/register').post((req, res, next) => {
+router.route('/register').post(async (req, res, next) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+    } catch {
+        return res.status(500).send();
+    }
+
     userSchema.create(req.body, (error, data) => {
         if (error) {
             return next(error);
