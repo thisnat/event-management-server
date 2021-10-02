1d81ec1087761563e7893340254bf27736c975e8
Nattawut <thisnattawut@gmail.com>
2021-10-02 20:59:19 +0700

    add update zone route

--- src/models/Reserve.js
+++ src/models/Reserve.js
@@ -17,10 +17,6 @@ let reserveSchema = new Schema({
     pic: {
         type: String
     },
-    isZoneSet: {
-        type: Boolean,
-        default: false
-    },
     create_at: {
         type: Date,
         default: Date.now()
--- src/routers/ZoneRouter.js
+++ src/routers/ZoneRouter.js
@@ -3,16 +3,26 @@ const express = require('express');
 const router = express.Router();
 const ZoneSchema = require('../models/Zone');
 
-//const auth = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
 router.get('/zoneByEvent/:id', (req, res, next) => {
-    ZoneSchema.find({eventId : req.params.id}, (error, data) => {
-        if(error){
+    ZoneSchema.find({ eventId: req.params.id }, (error, data) => {
+        if (error) {
             return next(error);
         } else {
             res.send(data)
         }
     });
+});
+
+router.patch('/update/:id', auth, (req, res, next) => {
+    ZoneSchema.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.send(data)
+        }
+    })
 });
 
 module.exports = router;
