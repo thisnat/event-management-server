d75bb6f8679db145beafa6d19cbc634e0b12dae0
Nattawut <thisnattawut@gmail.com>
2021-10-06 19:49:36 +0700

    add payment confirm & cancel route

--- src/routers/PaymentRouter.js
+++ src/routers/PaymentRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const PaymentSchema = require('../models/Payment');
-
+const EventSchema = require('../models/Event');
+const ZoneSchema = require('../models/Zone');
 const auth = require('../middleware/auth');
 
 router.get('/user', auth, (req, res, next) => {
@@ -44,5 +44,28 @@ router.get('/:id', auth, (req, res, next) => {
         }
     })
 });
+
+router.patch('/confirm/:id', auth, (req, res, next) => {
+    PaymentSchema.findByIdAndUpdate(req.params.id, {status : 2, update_at : Date.now()}, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.send(data)
+        }
+    })
+});
+
+router.patch('/cancel/:id', auth, (req, res, next) => {
+    PaymentSchema.findByIdAndUpdate(req.params.id, {status : 3}, async (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            await EventSchema.findByIdAndUpdate(data.eventId, { $inc: { reserve: -1 } });
+            await ZoneSchema.findByIdAndUpdate(data.zoneId, {owner : "", isReserve : false});
+            res.send(data)
+        }
+    })
+});
+
 
 module.exports = router;
